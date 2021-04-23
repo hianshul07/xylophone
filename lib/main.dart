6af7b287383497e5c145708dcc5d7e751b9ca0c1
hianshul07 <hianshul07@gmail.com>
2021-04-23 16:06:21 +0530

    second one

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,86 @@
-import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  void playSound(int noteNumber) {
+    final player = AudioCache();
+    player.play('assets_note$noteNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
           child: Center(
-            child: MaterialButton(
-              onPressed: () {
-                AudioPlayer audio = AudioPlayer();
-                audio.play('assets_note1.wav');
-              },
-              child: Text('Henlo'),
+            child: Expanded(
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: MaterialButton(
+                      color: Colors.red,
+                      onPressed: () {
+                        playSound(1);  
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: MaterialButton(
+                      color: Colors.orange,
+                      onPressed: () {
+                        playSound(2);
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: MaterialButton(
+                      color: Colors.yellow,
+                      onPressed: () {
+                        playSound(3);
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: MaterialButton(
+                      color: Colors.green,
+                      onPressed: () {
+                        playSound(4);
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: MaterialButton(
+                      color: Colors.teal,
+                      onPressed: () {
+                        playSound(5);
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: MaterialButton(
+                      color: Colors.blue,
+                      onPressed: () {
+                        playSound(6);
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: MaterialButton(
+                      color: Colors.purple,
+                      onPressed: () {
+                        playSound(7);
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
